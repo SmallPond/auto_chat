92e2096b219e755412b60801a41d413bb17bffeb
SmallPond <645581635@qq.com>
2017-07-17 16:23:54 +0800

    在配置文件中可使用‘#‘‘#’开头进行注释

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 *version：v0.1
 ###程序功能：基于itchat的微信定时发送消息
-
+###程序说明：对配置文件task_file.txt进行配置时，可使用‘#’开头注释一行文字
 ##使用方法：
 1.在文件task_file.txt中进行配置
 ![](http://i.imgur.com/cu9WGIR.png)
--- chat.py
+++ chat.py
@@ -13,13 +13,12 @@ _set_time = "11：00"
 _send_message = "It's time to go"
 #_task_file = sys.argv[1]
 _task_file = 'task_file.txt'
+
 def login_chat_init():
 
     # 登录微信， hotReload热登录，一定时间内不需在扫描验证码
     itchat.auto_login(hotReload=True)
     # 获取备注、微信号、昵称中的任何一项等于name键值的用户
-    #baby = itchat.search_friends(name=Contacts)[0]
-    #print(baby)
     # 不含参数则获取自己的信息
     #I = itchat.search_friends()
     #I.send("hello")
@@ -35,10 +34,15 @@ def get_task():
     '''
     taskList = []
     fileHander = open(_task_file, 'r', encoding='utf-8')      # 读取任务配置文件
-    fileHead = fileHander.readline()                          # 读取说明行
     fileList = fileHander.readlines()
     for line in fileList:
-        taskList.append(line.split())                                     # 以空格分开
+        if line[0] == '#':                                   # 配置文件中可使用‘#’开头注释一行
+            continue
+        else:
+            if len(line) > 6:                               # 时间 对象 信息 至少为6个长度
+                taskList.append(line.split())                # 以空格分开
+            else:
+                continue                                    # 跳过空行和不符合规定的行
     return taskList
 
 
@@ -78,9 +82,6 @@ if __name__ == '__main__':
     login_chat_init()
     taskList = get_task()
     start_task(taskList)
-    #contact = itchat.search_friends(name='dsa')
-    #contact = itchat.search_friends(name='一个宝贝儿')
-    #print(contact)
-    #contact.send('da')
+
 
 
